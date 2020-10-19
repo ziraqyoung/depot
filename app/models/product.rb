54f89a2e042c94cc2447078e6d653331ff1f1382
ziraqyoung <ziraqyoung@outlook.com>
2020-10-19 15:47:28 +0300

    Smarter cart - Add quantity attr to line items - define add_product method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :line_items
 
   before_destroy :ensure_not_referenced_by_any_lineitem
-  
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
