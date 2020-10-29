4ddb1b7045accb2781616e97ca0b29f1b881b00a
ziraqyoung <ziraqyoung@outlook.com>
2020-10-29 10:38:46 +0300

    Fix: remove trailing space

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
         format.json { render :show, status: :ok, location: @product }
 
         @products = Product.all.order(:title)
-        ActionCable.server.broadcast("products", 
+        ActionCable.server.broadcast("products",
                                      html: render_to_string("store/index", layout: false)
                                     )
       else
