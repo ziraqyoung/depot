d23b346ce1792a88b55bd13042bca5a483537ece
ziraqyoung <ziraqyoung@outlook.com>
2020-10-31 23:40:18 +0300

    Authorization - Restrict access via required login - Add test for authorization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,27 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+module AuthenticationHelpers
+  def login_as(user)
+    if respond_to? :visit
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+    else
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    login_as users(:one) 
+  end
+end
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -10,4 +31,12 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include AuthenticationHelpers
+end
+
+class ActionDispatch::SystemTestCase
+  include AuthenticationHelpers
 end
