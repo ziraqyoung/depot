2fb7044d4608f7cb70f143786448e9515596ffd5
ziraqyoung <ziraqyoung@outlook.com>
2020-11-01 11:50:55 +0300

    Ruby 2.7 deprecation warnings - Add separation of positional and keyword arguments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,7 +47,7 @@ class ProductsController < ApplicationController
 
         @products = Product.all.order(:title)
         ActionCable.server.broadcast("products",
-                                     html: render_to_string("store/index", layout: false)
+                                     { html: render_to_string("store/index", layout: false) }
                                     )
       else
         format.html { render :edit }
