dfa1bc35327291d0aabbbd1e4eaf2889cdcc2a12
ziraqyoung <ziraqyoung@outlook.com>
2020-11-02 12:34:03 +0300

    Add locale support - Create an initializer to set default locale to supported translations - Scope store front routes to use locale

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
   resources :users
-  resources :orders
-  resources :line_items
-  root 'store#index', as: 'store_index'
-  resources :carts
+
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index'
+  end
+
   resources :products do
     get :who_bought, on: :member
   end
